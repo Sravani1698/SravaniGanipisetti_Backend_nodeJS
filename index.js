99a22fc0198743a4f0d44f4e283a7a4a805a79dc
Sravani1698 <105673824+Sravani1698@users.noreply.github.com>
2023-12-06 02:36:49 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const http = require('http') ; 
+const fs = require('fs');
+const path = require('path');
+http.createServer((req, res) =>{
+    console.log(req.url);
+
+    if(req.url ==='/'){ // home page
+
+       fs.readFile(path.join(__dirname,'Public','index.html'),(err,content)=>{
+
+        if (err) throw err ;
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.end(content)
+
+       })
+    }
+   
+    else if (req.url === '/about'){
+        fs.readFile(path.join(__dirname,'Public','index.html'),(err,content)=>{
+
+            if (err) throw err ;
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.end(content)
+    
+           })
+    
+    }
+    else if (req.url ==='/api'){
+        fs.readFile(path.join(__dirname,'Public','universityDB.json'),(err,content)=>{
+
+            if (err) throw err ;
+            res.writeHead(200, {'Content-Type': 'application/json'})
+            res.end(content)
+    
+           })
+    
+    }
+    else{
+        res.end("<h1> 404 Nothing is here </h1>")
+    }
+
+    
+
+}).listen(1491,()=>console.log("Great our Server is running on port 1491"));
+
+ 
